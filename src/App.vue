b3a03d8bacbab8449289f17f223f20f8d96f5ab1
eldorado-22 <jumashevv992@gmail.com>
2023-04-18 00:30:29 +0600

    lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+
+      <div>Кол-вo лайков: <strong>{{ likes }}</strong> </div>
+      <div>Кол-вo дизлайков: <strong>{{ dislikes }}</strong> </div>
+      <div>
+          <button v-on:click="addLike">Like</button>
+          <button @click="addDisLike">Dislike</button>
+      </div>
+  </div>
 </template>
 
+<script>
+
+export default {
+    data() {
+        return{
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
+        addDisLike() {
+            this.dislikes += 1;
+        }
+    }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
