96c5cad0bc17f361a10295fa12626a93e849980e
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-04-18 05:33:14 +0600

    likes (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,96 @@
 <template>
-  <div>
-
-      <div>Кол-вo лайков: <strong>{{ likes }}</strong> </div>
-      <div>Кол-вo дизлайков: <strong>{{ dislikes }}</strong> </div>
-      <div>
-          <button v-on:click="addLike">Like</button>
-          <button @click="addDisLike">Dislike</button>
-      </div>
-  </div>
+    <div class="app">
+        <form @submit.prevent >
+            <h4>Создание поста</h4>
+            <input
+                v-bind:value="title"
+                @input="title = $event.target.value"
+                class="input"
+                type="text"
+                placeholder="Название"
+            >
+            <input
+                v-bind:value="body"
+                @input="body = $event.target.value"
+                class="input"
+                type="text"
+                placeholder="Описание"
+            >
+            <button
+                class="btn"
+                @clcik="createPost"
+            >
+                Создать</button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название:</strong> {{post.title}}</div>
+            <div><strong>Описание:</strong> {{post.body}}</div>
+        </div>
+    </div>
 </template>
 
 <script>
 
 export default {
     data() {
-        return{
-            likes: 0,
-            dislikes: 0,
+        return {
+           posts: [
+               {id: 1, title: "JavaScript", body: "Описание текста"},
+               {id: 2, title: "JavaScript 2", body: "Описание поста 2"},
+               {id: 3, title: "JavaScript 3", body: "Описание поста 3"},
+               {id: 4, title: "JavaScript 4", body: "Описание поста 4"},
+           ],
+            title: '',
+            body: '',
         }
     },
     methods: {
-        addLike() {
-            this.likes += 1;
+        createPost(event){
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
         },
-        addDisLike() {
-            this.dislikes += 1;
-        }
     }
 }
 
 </script>
 
 <style>
-
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app{
+    padding: 20px;
+}
+form{
+    display: flex;
+    flex-direction: column;
+}
+.btn{
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 2px solid teal;
+    border-radius: 5px;
+}
+.post{
+    padding: 15px;
+    border: 2px solid blue;
+    margin-top: 15px;
+}
+.input{
+    width: 100%;
+    border: 2px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
 </style>
